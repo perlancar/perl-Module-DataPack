3f7cb104423ae90a067873b93f415481027a8873
perlancar (@pc-office) <perlancar@gmail.com>
2015-06-12 14:22:57 +0700

    Fixes

--- lib/Module/DataPack.pm
+++ lib/Module/DataPack.pm
@@ -30,8 +30,12 @@ large. For example, in `App::pause`, the `pause` script embeds ~320 modules with
 a total of ~54000 lines. The overhead of fatpack code is about 49ms, while with
 datapack the overhead is about XXX ms.
 
-The downside of this technique compared to fatpack is that you cannot use
-__DATA__ section for other purpose (well, with some care, you can).
+There are two downsides of this technique. The major one is that you cannot load
+modules during BEGIN phase (e.g. using `use`) because at that point, DATA
+section is not yet available. You can only use run-time require()'s.
+
+Another downside of this technique is that you cannot use __DATA__ section for
+other purpose (well, actually with some care, you still can).
 
 _
     args_rels => {
@@ -202,10 +206,16 @@ sub datapack_modules {
     push @res, $args{preamble} if defined $args{preamble};
     push @res, <<'_';
 BEGIN {
-    my $fh = \*DATA;
-# INSERT_BLOCK: Data::Section::Seekable::Reader read_dss_toc
+    my $toc;
     unshift @INC, sub {
-        if (my $content = $toc{$_[1]}) {
+        $toc ||= do {
+            my $fh = \*DATA;
+# INSERT_BLOCK: Data::Section::Seekable::Reader read_dss_toc
+            \%toc;
+        };
+        if ($toc->{$_[1]}) {
+            seek DATA, $toc->{$_[1]}[0], 0;
+            read DATA, my($content), $toc->{$_[1]}[1];
             open my $fh, '<', \$content
                 or die "DataPacker error loading $_[1] (could be a perl installation issue?)";
             return $fh;
@@ -218,13 +228,16 @@ _
     push @res, $args{postamble} if defined $args{postamble};
 
     require Data::Section::Seekable::Writer;
-    my $writer = Data::Section::Seekable::Writer->new(separator => '---');
-    for my $mod (keys %module_srcs) {
-        my $mod_pm = $mod; $mod_pm =~ s!::!/!g; $mod_pm .= ".pm";
-        my $content = "# line 1 \"__FILE__\"\n" . $module_srcs{$mod};
+    my $writer = Data::Section::Seekable::Writer->new(separator => "---\n");
+    for my $mod_pm (keys %module_srcs) {
+        my $content = join(
+            "",
+            "# line 1 \"__FILE__\"\n",
+            $module_srcs{$mod_pm},
+        );
         $writer->add_part($mod_pm => $content);
     }
-    push @res, $writer;
+    push @res, "\n__DATA__\n", $writer;
 
     if ($args{output}) {
         my $outfile = $args{output};
